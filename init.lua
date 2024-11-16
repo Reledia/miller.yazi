b5d927fc3ea5776acccaebb1cda2dd5da72b23a9
Mirko <mirkopoj@hotmail.com>
2024-11-16 12:32:35 -0300

    Changed deprecated Paragraph to Text

--- init.lua
+++ init.lua
@@ -37,11 +37,11 @@ function M:peek()
 	if self.skip > 0 and i < self.skip + limit then
 		ya.manager_emit(
 			"peek",
-			{ tostring(math.max(0, i - limit)), only_if = tostring(self.file.url), upper_bound = "" }
+			{ math.max(0, i - limit), only_if = self.file.url, upper_bound = true }
 		)
 	else
 		lines = lines:gsub("\t", string.rep(" ", PREVIEW.tab_size))
-		ya.preview_widgets(self, { ui.Paragraph.parse(self.area, lines) })
+		Ra.preview_widgets(self, { ui.Text.parse(lines):area(self.area) })
 	end
 end
 
